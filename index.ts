4da461da1c882c779933a136467656aa0825fe0e
Sharjeel Ahmad Buledi <sharjeelbuledi1@gmail.com>
2024-04-01 01:26:37 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Value", type: "number", name: "firstNumber" },
+  {
+    message: "Enter Second Value",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Select Any Arthmetic Operation To Perform",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Select ANy Valid Operation");
+}
